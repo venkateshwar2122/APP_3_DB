da4297a22245cabbe03bdf72f08dcc9f6718e9c1
venkateshwar2122 <venkateshwar2122@gmail.com>
2025-04-02 09:16:38 +0530

    1

---
+++ index.js
@@ -0,0 +1,48 @@
+let mongoose = require("mongoose");
+let express = require("express");
+
+let schobj = new mongoose.Schema({
+    "_id": Number,
+    "name": String,
+    "age": Number,
+    "place": String,
+    "gen": String
+});
+
+let dm = mongoose.model("users", schobj); 
+
+mongoose.connect("mongodb://127.0.0.1:27017/hfs3db")
+    .then(() => {
+        console.log("connection okay");
+    })
+    .catch((err) => {  
+        console.log("error in db connection", err);
+    });
+
+let app = express();
+app.use(express.json());
+
+app.post("/add", (req, res) => {
+    let data = new dm(req.body);
+    data.save()
+        .then(() => {
+            res.send("data added");
+        })
+        .catch((err) => { 
+            res.send("error in storing data: " + err);
+        });
+});
+
+app.get("/", (req, res) => {
+    dm.find()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.send("error in getting data");
+        });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000/");
+});
